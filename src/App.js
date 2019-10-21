0906c1d2062ef7220735cb793af602c1675da392
meddlin <drushing.dev@gmail.com>
2019-10-21 17:39:22 -0500

    Pushing project. Should be able to clone and work from here, but README, etc. needs work.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
+const StyledButton = styled.button`
+  color: ${(props) => { 
+      if (props.customColor === 'yellow')
+        return 'white';
+
+      return props.customColor;
+    } 
+  };
+  background-color: black;
+  padding: 10px;
+`;
+
 function App() {
+  const handleClick = () => {
+    alert('clicked it!');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledButton 
+        onClick={handleClick}
+        customColor='yellow'>Click me!</StyledButton>
     </div>
   );
 }
